9b058a995b24061066b8b7efade3f664e3305b1a
amkotsar-prog <79314142+amkotsar-prog@users.noreply.github.com>
2021-05-06 16:27:42 +0300

    Update Lesson4Task6A.py

--- Lesson4Task6A.py
+++ Lesson4Task6A.py
@@ -8,13 +8,4 @@ for i in count(int(input('Введите  число начала итераци
 
 
 
-# a = int(input('Введите число начала итераций:  '))
-# for el in count(a):
-    # print(el)
 
-"""list_b = split(input('Введите значения списка для перебора: '))
-iter = cycle(list_b)
-print(next(iter))
-print(next(iter))
-print(next(iter))
-print(next(iter))"""
