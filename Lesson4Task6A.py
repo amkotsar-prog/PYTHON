f00e2781a37c5e5994da18556ddfe7f9f7893e37
Alexander Kotsar <alexkotsar@yahoo.com>
2021-05-06 16:30:47 +0300

    Edited the file

--- Lesson4Task6A.py
+++ Lesson4Task6A.py
@@ -5,16 +5,3 @@ for i in count(int(input('Введите  число начала итераци
     qui = input()
     if qui == 'q' :
         break
-
-
-
-# a = int(input('Введите число начала итераций:  '))
-# for el in count(a):
-    # print(el)
-
-"""list_b = split(input('Введите значения списка для перебора: '))
-iter = cycle(list_b)
-print(next(iter))
-print(next(iter))
-print(next(iter))
-print(next(iter))"""
